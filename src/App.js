3b7abd5186fd1da6fc6f5f812d348656e10c9570
Banti Prajapati <banti.prajapati@systematixindia.com>
2021-09-01 18:37:37 +0530

    add the useeffect for initialize the movie data without using button fetch, when page is load the directly load all data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] =useState(false);
   const [error, setError] = useState(null)
 
-   const fetchMovieHandler =async ()=> {
+   const fetchMovieHandler =useCallback(async ()=> {
     setIsLoading(true);
     try {
       setError(null);
@@ -32,8 +32,12 @@ function App() {
       setError(error.message)
     }
     setIsLoading(false);
+  }, []);
+  
+  useEffect(()=> {
+    fetchMovieHandler();
+  }, [fetchMovieHandler])
 
-  }
   //this is advance version of line 53 to 56 start
   let content = <p>Found no movie</p> 
   if(movies.length > 0) {
