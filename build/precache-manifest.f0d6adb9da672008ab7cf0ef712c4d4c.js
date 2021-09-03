612e6c7db3fe65d9b7347bbe14f5f716ee70cb70
Banti <bantiprajapati30@gmail.com>
2021-09-04 01:01:23 +0530

    commit after run build and make for live

---
+++ build/precache-manifest.f0d6adb9da672008ab7cf0ef712c4d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb27f6a02442d0bf9c3e1762524160e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10bff6ccebb1e0a8b800",
+    "url": "/static/css/main.d8a6448e.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bff6ccebb1e0a8b800",
+    "url": "/static/js/main.ffa0b911.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
